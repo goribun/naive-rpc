c94e2336ebe9bc3179a2f7ffeb88aa650657631c
陈川 <chenchuan@autohome.com.cn>
2017-04-12 10:55:39 +0800

    提交代码

--- naive-client/src/test/java/com.goribun.navie.client/proxy/RpcProxtTest.java
+++ naive-client/src/test/java/com.goribun.navie.client/proxy/RpcProxtTest.java
@@ -1,6 +1,7 @@
 package com.goribun.navie.client.proxy;
 
 import com.goribun.navie.client.poxy.RpcProxy;
+import com.goribun.navie.facade.dto.UserDTO;
 import com.goribun.navie.facade.intefaces.IUserService;
 import org.junit.Test;
 
@@ -11,8 +12,17 @@ import org.junit.Test;
  */
 public class RpcProxtTest {
     @Test
-    public void testRpcProxy(){
-        IUserService userService = RpcProxy.getProxy("127.0.0.1",8080,IUserService.class);
+    public void testRpcProxy() {
+        IUserService userService = RpcProxy.getProxy("127.0.0.1", 8080, IUserService.class);
         userService.getUsers();
     }
+
+    @Test
+    public void testRpcProxy_void() {
+        IUserService userService = RpcProxy.getProxy("127.0.0.1", 8080, IUserService.class);
+        UserDTO userDTO = new UserDTO();
+        userDTO.setName("陈大川").setId(3);
+        userService.addUser(userDTO);
+    }
+
 }

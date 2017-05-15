bfbc68da888d14ba626b50636432e8a181adb4f2
王雪松 <xswang@outlook.com>
2017-05-15 23:18:40 +0800

    暂时不抽取

---
+++ naive-core/src/main/java/com/goribun/navie/core/utils/DeserializeUtil.java
@@ -0,0 +1,22 @@
+package com.goribun.navie.core.utils;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+
+/**
+ * TODO 反序列化工具
+ *
+ * @author wangxuesong
+ */
+public class DeserializeUtil {
+
+    private DeserializeUtil() {
+    }
+
+
+    private List deserialize(String rawParams, Type[] paramTypes) throws IOException {
+        return null;
+    }
+
+}
